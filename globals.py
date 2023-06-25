8d9ddfaa5087fed16eb8ae9fa178197e11190c5c
István Gy. Zsély <zselyigy@gmail.com>
2023-06-25 21:59:58 +0200

    Keyboard and mouse action handling restructured.

--- globals.py
+++ globals.py
@@ -1,6 +1,6 @@
 def global_init():
     global path, sel_path, start_c, end_c, fin_path, alg_s, centre_x, centre_y, alg_sp, solved_text, path_nmbr, c, percentage, time, start_t, timer_r, screen, zoom_in, zoom_out
-    global setup_screen_fontsize, setup_screen_font_color, setup_screen_bg_color, sc_x, sc_y, mc_x, mc_y, gamemode_solvethemaze, gamemode_timelimited, gamemode_speedrun, gamemode_text, kbm_click_and_drag
+    global setup_screen_fontsize, setup_screen_font_color, setup_screen_bg_color, sc_x, sc_y, mc_x, mc_y, gamemode_solvethemaze, gamemode_timelimited, gamemode_speedrun, gamemode_text, kbmaction_text
     path = (255, 255, 255)
     sel_path = (107, 216, 255)
     start_c = (63, 192, 76)
@@ -33,4 +33,4 @@ def global_init():
     gamemode_speedrun = 'Speed run'
     gamemode_text = 'a'
 # Keyboard and mouse behaviour
-    kbm_click_and_drag = True
+    kbmaction_text = "kbm_click_and_drag"
--- main.py
+++ main.py
@@ -268,7 +268,7 @@ def main():
             elif pygame.mouse.get_pressed()[0] == True:
                 mazex = math.floor((event.pos[0] - globals.sc_x) / zoom + globals.mc_x - offset_x + 0.5)
                 mazey = math.floor((event.pos[1] - globals.sc_y) / zoom + globals.mc_y - offset_y + 0.5)
-                if globals.kbm_click_and_drag:
+                if globals.kbmaction_text == "kbm_click_and_drag":
                     if mazex > -1 and mazex < 2 * rows + 1 and mazey > -1 and mazey < 2 * cols + 1:
                         if sqmaze[mazex][mazey] == 1:    # the tile is empty. check if selectable or not
                             if (pathmaze[mazex - 1][mazey] + pathmaze[mazex + 1][mazey] + pathmaze[mazex][mazey - 1] + pathmaze[mazex][mazey + 1]) == 1:
@@ -300,7 +300,7 @@ def main():
                                     globals.timer_r = 0
                                     pygame.display.flip()
             elif pygame.mouse.get_pressed()[2] == True:
-                if globals.kbm_click_and_drag:
+                if globals.kbmaction_text == "kbm_click_and_drag":
                     mazex = math.floor((event.pos[0] - globals.sc_x) / zoom + globals.mc_x - offset_x + 0.5)
                     mazey = math.floor((event.pos[1] - globals.sc_y) / zoom + globals.mc_y - offset_y + 0.5)
                     if mazex > -1 and mazex < 2 * rows + 1 and mazey > -1 and mazey < 2 * cols + 1:
